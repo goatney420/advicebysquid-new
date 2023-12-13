5fc8ce941d9e3a0b77309270d519e4d315a00243
goatney420 <84412113+goatney420@users.noreply.github.com>
2023-12-12 16:53:14 -0800

    edited database cuz im smart

--- my-app/src/EmotionsData.js
+++ my-app/src/EmotionsData.js
@@ -2,32 +2,32 @@ export const EmotionsData = [
     {
         id:0,
         name: "select one",
-        watch:"https://www.youtube.com/watch?v=Lt1u6N7lueM",
-        read:"https://en.wikipedia.org/wiki/Man%27s_Search_for_Meaning"
+        watch:"",
+        read:""
     },
     {
         id:1,
         name: "chonky",
-        watch:"https://www.youtube.com/watch?v=Lt1u6N7lueM",
+        watch:"https://www.youtube.com/watch?v=eoV7lw7YBG4",
         read:"https://en.wikipedia.org/wiki/Man%27s_Search_for_Meaning"
     },
     {
         id:2,
         name: "groovy",
-        watch:"https://www.youtube.com/watch?v=Lt1u6N7lueM",
-        read:"https://en.wikipedia.org/wiki/Man%27s_Search_for_Meaning"
+        watch:"https://www.youtube.com/watch?v=ynkaGd_zGwE",
+        read:"https://www.amazon.com/Unhoneymooners-Christina-Lauren/dp/1501128035"
     },
     {
         id:3,
         name: "emo",
-        watch:"https://www.youtube.com/watch?v=Lt1u6N7lueM",
-        read:"https://en.wikipedia.org/wiki/Man%27s_Search_for_Meaning"
+        watch:"https://www.youtube.com/watch?v=v4pi1LxuDHc&pp=ygUbc2xlZXBpbmcgb24gdGhlIGZsb29yIG11c2lj",
+        read:"https://www.penguinrandomhouse.com/the-read-down/sad-fiction/"
     },
     {
         id:4,
         name: "hungry",
         watch:"https://www.youtube.com/watch?v=Lt1u6N7lueM",
-        read:"https://en.wikipedia.org/wiki/Man%27s_Search_for_Meaning"
+        read:"https://www.bonappetit.com/recipe/butter-basted-rib-eye-with-crunchy-fennel-salad"
     },
     
 ]   
